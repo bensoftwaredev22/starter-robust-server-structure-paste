243f9f4a56d4f8d5149c3f294141e023047b5c58
Benjamin <benver17@gmail.com>
2022-07-25 20:20:28 -0500

    Added controller and router files for pastes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
-const pastes = require("./data/pastes-data");
+//const pastes = require("./data/pastes-data");
+const pastesRouter = require("./pastes/pastes.router");
 // TODO: Follow instructions in the checkpoint to implement ths API.
 
 app.use(express.json()) //express.json() function is a built-in middleware that adds a body property to the request (req.body). 
 
+/* Added this as a function to pastes.controller.js
 app.use("/pastes/:pasteId", (req, res, next) => {
   const { pasteId } = req.params;
   const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
@@ -16,41 +18,16 @@ app.use("/pastes/:pasteId", (req, res, next) => {
     next({ status: 404, message: `Paste id not found: ${pasteId}` });
   }
 });
+*/
+app.use("/pastes", pastesRouter); // Note: app.use
+/*
 app.get("/pastes", (req, res) => {
   res.json({ data: pastes });
 });  // Modified the existing handler for /pastes to handle only GET requests(35.3).
+*/
 
-// New middleware function to validate the request body
-function bodyHasTextProperty(req, res, next) {
-  const { data: { text } = {} } = req.body;
-  if (text) {
-    return next(); // Call `next()` without an error message if the result exists
-  }
-  next({
-    status: 400,
-    message: "A 'text' property is required.",
-  });
-}
 
-// Variable to hold the next ID
-// Because some IDs may already be used, find the largest assigned ID
-let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
 
-app.post("/pastes", bodyHasTextProperty, (req, res, next) => {
-  const { data: { name, syntax, exposure, expiration, text, user_id } = {} } = req.body;
-
-  const newPaste = {
-    id: ++lastPasteId, // Increment last ID, then assign as the current ID
-    name,
-    syntax,
-    exposure,
-    expiration,
-    text,
-    user_id,
-  };
-  pastes.push(newPaste);
-  res.status(201).json({ data: newPaste });
-});
 
 // Not found handler
 app.use((request, response, next) => {
@@ -63,14 +40,5 @@ app.use((error, request, response, next) => {
   response.status(status).json({ error: message });
 });
 
-
-// TODO: return an array of users from /users in form of { data: Array }
-// TODO: return a single user by id from /users/:userId in form of { data: Object }
-
-// TODO: return all states from /states in the form of { data: Array }
-// TODO: Return a single state from /states/:stateCode in the form of { data: { stateCode: String, name: String } }
-
-// TODO: add not-found handler
-// TODO: Add error handler
 
 module.exports = app;
